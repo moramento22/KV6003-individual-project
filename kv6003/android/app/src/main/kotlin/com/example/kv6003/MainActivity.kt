99cf284f5bbb7383cf98abc6c186ca7430161073
Patryk Wieczorek <moramento22@gmail.com>
2023-03-08 21:36:28 +0000

    Initial commit and Landing Page

---
+++ kv6003/android/app/src/main/kotlin/com/example/kv6003/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kv6003
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ kv6003/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'color_schemes.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'treeTRIP',
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      //darkTheme: ThemeData(
+      //  useMaterial3: true,
+      //colorScheme: darkColorScheme
+      //),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Theme.of(context).colorScheme.background,
+      systemNavigationBarColor: Theme.of(context).colorScheme.background,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: const SafeArea(
+            child: Center(
+                child: StartupCard()
+            )
+        )
+    );
+  }
+}
+
+class StartupCard extends StatelessWidget {
+  const StartupCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Theme.of(context).colorScheme.surfaceVariant,
+      child: Center(
+          child: RichText(
+            text: TextSpan(
+              children: <TextSpan>[
+                TextSpan(text: 'tree', style: GoogleFonts.robotoSerif(fontStyle: FontStyle.italic, fontSize: 57, fontWeight: FontWeight.w900, color: Theme.of(context).colorScheme.onSurfaceVariant)),
+                TextSpan(text: 'TRIP', style: GoogleFonts.roboto(fontWeight: FontWeight.w600, fontSize: 57, color: Theme.of(context).colorScheme.onSurfaceVariant)),
+              ]
+            )
+          ),
+      )
+    );
+  }
+}
