b59c6e93faae3f9fc440d929c64338852cceec5e
Patryk Wieczorek <moramento22@gmail.com>
2023-03-09 00:29:16 +0000

    Added second page and initial transition between the two

--- kv6003/lib/main.dart
+++ kv6003/lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:kv6003/search_page.dart';
 import 'color_schemes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,54 +17,60 @@ class MyApp extends StatelessWidget {
       title: 'treeTRIP',
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LandingPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LandingPage> createState() => _LandingPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LandingPageState extends State<LandingPage> {
+  @override
+  void initState() {
+
+    route() {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SearchPage()));
+    }
+
+    startTime() async {
+      var duration = const Duration(seconds: 5);
+      return Timer(duration, route);
+    }
+
+    super.initState();
+    startTime();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     var brightness = MediaQuery.of(context).platformBrightness;
     bool isDarkMode = brightness == Brightness.dark;
 
-    if(isDarkMode){
+    if (isDarkMode) {
       SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarColor: Theme.of(context).colorScheme.background,
-        systemNavigationBarColor: Theme.of(context).colorScheme.background,
-        systemNavigationBarIconBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light
-      ));
-    }
-    else{
+          statusBarColor: Theme.of(context).colorScheme.background,
+          systemNavigationBarColor: Theme.of(context).colorScheme.background,
+          systemNavigationBarIconBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.light));
+    } else {
       SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
           statusBarColor: Theme.of(context).colorScheme.background,
           systemNavigationBarColor: Theme.of(context).colorScheme.background,
           systemNavigationBarIconBrightness: Brightness.dark,
-          statusBarIconBrightness: Brightness.dark
-      ));
+          statusBarIconBrightness: Brightness.dark));
     }
 
     return Scaffold(
         extendBodyBehindAppBar: true,
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: const SafeArea(
-            child: Center(
-                child: StartupCard()
-            )
-        )
-    );
+        body: const SafeArea(child: Center(child: StartupCard())));
   }
 }
 
@@ -72,18 +80,26 @@ class StartupCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 0,
-      color: Theme.of(context).colorScheme.surfaceVariant,
-      child: Center(
+        elevation: 0,
+        color: Theme.of(context).colorScheme.surfaceVariant,
+        child: Center(
           child: RichText(
-            text: TextSpan(
-              children: <TextSpan>[
-                TextSpan(text: 'tree', style: GoogleFonts.robotoSerif(fontStyle: FontStyle.italic, fontSize: 57, fontWeight: FontWeight.w900, color: Theme.of(context).colorScheme.onSurfaceVariant)),
-                TextSpan(text: 'TRIP', style: GoogleFonts.robotoFlex(fontWeight: FontWeight.w600, fontSize: 57, color: Theme.of(context).colorScheme.onSurfaceVariant)),
-              ]
-            )
-          ),
-      )
-    );
+              text: TextSpan(children: <TextSpan>[
+            TextSpan(
+                text: 'tree',
+                style: GoogleFonts.robotoSerif(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 57,
+                    fontWeight: FontWeight.w900,
+                    color: Theme.of(context).colorScheme.onSurfaceVariant)),
+            TextSpan(
+                text: 'TRIP',
+                style: GoogleFonts.robotoFlex(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 57,
+                    color: Theme.of(context).colorScheme.onSurfaceVariant)),
+          ])),
+        ));
   }
 }
+
